632162c60799f3bfb640d5f42a97df63fd463e64
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-06 13:29:19 +0000

    Implemet changes requested by the reviewer

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,22 +1,33 @@
 const hamburger = document.querySelector('.menu-bars');
 const mobileNav = document.querySelector('#mobile-nav');
-// const navLInks = document.querySelectorAll('.nav-link');
-// let display = document.querySelector(".display")
+const navLInks = document.querySelectorAll('.nav-link');
+const logo = document.querySelector('#logo');
+const closeButton = document.querySelector('.close-btn');
 
 hamburger.addEventListener('click', () => {
-  // mobileNav.style.display = display
   // toggle mobileNav when hamburger is clicked
-
-  if (mobileNav.style.display === 'none') {
     mobileNav.style.display = 'block';
-  } else {
-    mobileNav.style.display = 'none';
-  }
-});
+    logo.style.display = 'none';
+  closeButton.style.display = 'block';
+  hamburger.style.display = 'none';
+  });
+
+  // When the close button is clicked
+closeButton.addEventListener('click', () => {
+  mobileNav.style.display = 'none';
+  logo.style.display = 'block';
+  hamburger.style.display = 'block';
+  closeButton.style.display = 'none';
+  })
+
 
 // close mobileNav when navLink is clicked
-// navLInks.forEach(link => {
-//   link.addEventListener("click", () => {
-//     mobileNav.style.display = "none";
-//   })
-// })
+navLInks.forEach(link => {
+  link.addEventListener("click", () => {
+    mobileNav.style.display = "none";
+    logo.style.display = "block";
+    hamburger.style.display = "block";
+  closeButton.style.display = 'none';
+
+  })
+})
